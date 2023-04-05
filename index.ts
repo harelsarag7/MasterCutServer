bdbaf058b325ea96e98cadfe08a836be0f95f8ba
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2023-04-05 14:57:56 +0300

    back to original

--- index.ts
+++ index.ts
@@ -19,8 +19,6 @@ server.use(uploadRouter);
 
 server.use(catchAll);
 
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+server.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}...`);
 });
